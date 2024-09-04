67f4385dba1eb35fe8775d1c5e31caae1cffb21f
IceBreaker <icebreaker@mac.local>
2024-09-03 22:52:40 -0400

    We have a working static example, which is creating visits, searches and orders

--- aligator/scenarios/static/models/visits.py
+++ aligator/scenarios/static/models/visits.py
@@ -6,13 +6,55 @@ from aligator.faker import faker
 from faker import Faker
 
 
+class UserType:
+    NEW_USER = 'new_user'
+    RETURNING_USER = 'returning_user'
+    REGISTERED_USER = 'registered_user'
+
+
+
 class Visit(db.Base):
     __tablename__ = 'visits'
     id = Column(Integer, primary_key=True)
     user_agent = Column(String)
+    user_type = Column(String)
     created_at = Column(DateTime)  # This will set the current timestamp on creation
 
     @staticmethod
-    def create_it(dt: datetime.datetime) -> 'Visit':
+    def create_it(dt: datetime.datetime, user_type) -> 'Visit':
     #     #todo: find a better way to make fake users (maybe use name as part of email + make sure cities are consistant)
-        return Visit(user_agent=faker.user_agent(), created_at=dt)
+        return Visit(
+            user_type=user_type,
+            user_agent=faker.user_agent(),
+            created_at=dt
+        )
+
+
+class Search(db.Base):
+    __tablename__ = 'searches'
+    id = Column(Integer, primary_key=True)
+    visit_id = Column(Integer)
+    created_at = Column(DateTime)  # This will set the current timestamp on creation
+
+    @staticmethod
+    def create_it(dt: datetime.datetime, visit_id) -> 'Visit':
+        #     #todo: find a better way to make fake users (maybe use name as part of email + make sure cities are consistant)
+        return Search(
+            visit_id=visit_id,
+            created_at=dt
+        )
+
+
+class Order(db.Base):
+    __tablename__ = 'orders'
+    id = Column(Integer, primary_key=True)
+    search_id = Column(Integer)
+    created_at = Column(DateTime)  # This will set the current timestamp on creation
+
+    @staticmethod
+    def create_it(dt: datetime.datetime, search_id) -> 'Visit':
+        #     #todo: find a better way to make fake users (maybe use name as part of email + make sure cities are consistant)
+        return Order(
+            search_id=search_id,
+            created_at=dt
+        )
--- aligator/scenarios/static/scenerio.py
+++ aligator/scenarios/static/scenerio.py
@@ -1,7 +1,7 @@
 from aligator.scenario import Scenario as ScenarioInterface, ScenarioParams as ScenarioParamsInterface
-from aligator.scenarios.static.models.visits import Visit
-
+from aligator.scenarios.static.models.visits import Visit, UserType, Search, Order
 from datetime import datetime, timedelta
+import numpy as np
 
 class DayIterator:
     def __init__(self, start_date: str, end_date: str):
@@ -34,9 +34,43 @@ class ScenarioParams(ScenarioParamsInterface):
     ScenarioParams is a class that holds the parameters for the scenario
     """
 
-    visits = 5
-    """visits is a parameter that holds the number of visits"""
+    seed = None
+    """seed is a parameter that holds the seed for the random number generator"""
 
+    min_visits = 5
+    """min number of visits is a parameter that holds the number of visits"""
+
+    visit_factor = 10
+    """multiplication of visit to have total"""
+
+
+    user_type_predictions = {
+        UserType.NEW_USER: 0.3,
+        UserType.RETURNING_USER: 0.1,
+        UserType.REGISTERED_USER: 0.6
+    }
+    """user_type_predictions is a parameter that holds the predictions for the user types"""
+
+    search_predictions = {
+        UserType.NEW_USER: 0.4,
+        UserType.RETURNING_USER: 0.6,
+        UserType.REGISTERED_USER: 0.9
+    }
+    """search_predictions is a parameter that holds the predictions for the search types"""
+
+    order_predictions = {
+        UserType.NEW_USER: 0.1,
+        UserType.RETURNING_USER: 0.6,
+        UserType.REGISTERED_USER: 0.8
+    }
+    """order_predictions is a parameter that holds the predictions for the order types"""
+
+    registration_predictions = {
+        UserType.NEW_USER: 0.5,
+        UserType.RETURNING_USER: 0.8,
+        UserType.REGISTERED_USER: 0.0
+    }
+    """registration_predictions is a parameter that holds the predictions for the registration"""
 
 class Scenario(ScenarioInterface):
 
@@ -46,28 +80,31 @@ class Scenario(ScenarioInterface):
             self.scenario_params = ScenarioParams()
         else:
             self.scenario_params = scenario_params
-
-
     def init_db(self):
         self.app.db.Base.metadata.create_all(self.app.db.engine)
 
 
+    @property
+    def seed(self):
+        return self.scenario_params.seed if self.scenario_params.seed else np.random.randint(10000000)
+
+
 #1 - Initialise database + tables
-
-
     def run_scenario(self):
         self.init_db()
+
         #iterate over days to  create visits for each day where we provide start date
 
         start_date = "2024-01-01"
-        end_date = "2024-01-10"
+        end_date = "2024-04-01"
         self.app.log.info("Running custom scenario")
         self.app.log.info(f"Params: {self.scenario_params}")
 
         for day in DayIterator(start_date, end_date):
-            for _ in range(self.scenario_params.visits):
-                visit = Visit.create_it(dt=day)
-                self.app.db.add(visit)
+            self.generate_day(dt=day)
+            # for _ in range(self.scenario_params.visits):
+            #     visit = Visit.create_it(dt=day)
+            #     self.app.db.add(visit)
 
             self.app.db.commit()
             #todo: add user creted
@@ -77,5 +114,47 @@ class Scenario(ScenarioInterface):
 
         pass
 
-    def get_visits(self):
-        return self.scenario_params.visits
+
+    def generate_day(self, dt):
+        for _ in range(
+                max(
+                    np.random.randint(self.scenario_params.visit_factor * self.scenario_params.min_visits),
+                    self.scenario_params.min_visits)
+        ):
+            self.generate_visit(dt)
+            self.app.db.commit()
+        pass
+
+    def generate_visit(self, dt):
+        user_type = np.random.choice(
+            list(self.scenario_params.user_type_predictions.keys()),
+            p=list(self.scenario_params.user_type_predictions.values())
+        )
+
+        visit = Visit.create_it(dt=dt, user_type=user_type)
+        self.app.db.add(visit)
+
+        if self.scenario_params.search_predictions[user_type] > np.random.rand():
+            self.generate_search(dt=dt, visit=visit)
+        else:
+            self.app.log.info(f'No search for {visit.user_type}')
+
+    def generate_search(self, dt, visit):
+        search = Search.create_it(dt=dt, visit_id=visit.id)
+        self.app.db.add(search)
+        self.app.log.info(f'Generating search + {visit.user_type}')
+        if self.scenario_params.order_predictions[visit.user_type] > np.random.rand():
+            self.generate_order(dt, visit, search)
+        else:
+            self.app.log.info(f'No order for {visit.user_type}')
+
+    def generate_order(self, dt, visit, search):
+        order = Order.create_it(dt=dt, search_id=search.id)
+        self.app.db.add(order)
+        self.app.log.info(f'Generating order + {visit.user_type}')
+
+"""
+visit -> search -> order 
+100 -> 60% -> 1%
+
+"""
